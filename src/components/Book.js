875176f7cc387f783a94498cd1a4788ec631ba39
Bex B <bexb1@hotmail.com>
2020-04-13 15:09:15 +0100

    REFACTOR: Move JSX into newly-created components.

---
+++ src/components/Book.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ShelfChanger from "./ShelfChanger";
+
+function Book(props) {
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage:
+                'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")'
+            }}
+          ></div>
+          <ShelfChanger />
+        </div>
+        <div className="book-title">Book title</div>
+        <div className="book-authors">Book author</div>
+      </div>
+    </li>
+  );
+}
+
+export default Book;

6958638be5141dde226d4fc241cdd12fc7a15da7
Bex B <bexb1@hotmail.com>
2020-05-08 10:17:46 +0100

    fix: grey thumbnail placeholder now shows again for books without a thumbnail image. ShelfChanger component shows on such books again also.

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,35 +7,33 @@ class Book extends Component {
     return (
       <li>
         <div className="book">
-          {book.imageLinks && (
-            <div className="book-top">
-              <div
-                className="book-cover"
-                style={{
-                  width: 128,
-                  height: 193,
-                  backgroundColor: "gray",
-                  backgroundImage: book.imageLinks
-                    ? `url(${book.imageLinks.thumbnail})`
-                    : "none",
-                }}
-              ></div>
-              <div className="book-shelf-changer">
-                <select
-                  value={book.shelf || shelf}
-                  onChange={(e) => onSelectShelf(e.target.value, book)}
-                >
-                  <option value="move" disabled>
-                    Move to...
-                  </option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-              </div>
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundColor: "gray",
+                backgroundImage: book.imageLinks
+                  ? `url(${book.imageLinks.thumbnail})`
+                  : "none",
+              }}
+            ></div>
+            <div className="book-shelf-changer">
+              <select
+                value={book.shelf || shelf}
+                onChange={(e) => onSelectShelf(e.target.value, book)}
+              >
+                <option value="move" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
             </div>
-          )}
+          </div>
           <div className="book-title">{book.title}</div>
           <div className="book-authors">
             {book.authors && book.authors.join(", ")}
